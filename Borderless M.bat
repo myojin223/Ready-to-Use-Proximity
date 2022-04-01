2e9c849c4bd7be817be1fa97eabf44a0728128a1
myojin223 <64243555+myojin223@users.noreply.github.com>
2022-04-01 09:51:11 -0600

    Updated batch file and xml to show reminder text on borderless M template

--- Borderless M.bat
+++ Borderless M.bat
@@ -1,2 +1,2 @@
-java -jar proximity.jar --cards=Decklist.txt --art_source=BEST --reminder_text=false --template=BorderlessM --copyright=false --use_card_back=false --flavor_text=false --watermark=none
+java -jar proximity.jar --cards=Decklist.txt --art_source=BEST --reminder_text=true --template=BorderlessM --use_card_back=false --watermark=none
 cmd /k
